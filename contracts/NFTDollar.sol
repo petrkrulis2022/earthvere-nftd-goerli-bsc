641ebe08a4e124f5ebf0f84533c14f255ece2cb1
karagyozovLT <georgi.karagyozov@looming.tech>
2023-01-19 02:38:30 +0200

    Create an INFTDollar interface and use it

--- contracts/NFTDollar.sol
+++ contracts/NFTDollar.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.17;
 
 import "./Ownable.sol";
+import "./interfaces/INFTDollar.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract NFTDollar is ERC20Burnable, Ownable {
+contract NFTDollar is INFTDollar, ERC20Burnable, Ownable {
   constructor() ERC20("NFT Dollar", "NFTD") {}
 
   function mint(address to, uint256 amount, uint256 decimalsOfInput) external {
